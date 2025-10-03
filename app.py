010a59efa617b3e3dbe819d4d129727f5dbd5245
Powell Ndlovu <137525355+kabila96@users.noreply.github.com>
2025-10-03 23:21:34 +0200

    app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import plotly.express as px
 
 
 # ----- Page config (do this ONCE and first) -----
+st.set_page_config(page_title="POWELL NDLOVU ENVIRONMENTAL DATA ANALYST", layout="wide")
 st.set_page_config(page_title="Tech Stocks Analysis Dashboard", layout="wide")
 
 # ----- Title -----
@@ -157,4 +158,5 @@ if problems:
     with st.expander("⚠️ Data loading notes"):
         for msg in problems:
             st.write("- " + msg)
+
 
